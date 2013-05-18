55e0e420872080320bb506c448efe12d4cb47ee1
César Rodríguez González <cesar.rgon@gmail.com>
2013-05-18 23:44:58 +0100

    Primer commit pagina web

---
+++ README.md
@@ -0,0 +1,4 @@
+# Xfce Installer Web
+Pagina web asociada al script xfce-installer generada por GitHub Pages usando el tema Architect de Jason Long.
+
+Modificaciones de diseno realizadas por Isidro Rodriguez Gonzalez y Cesar Rodriguez Gonzalez.
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+// Evita errores de consola en exploradores que no las tiene.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Visit www.menucool.com for source code, other menu scripts and web UI controls. Please keep this notice intact. Thank you.
+var sse2=function(){var c=20;var b,a;return{buildMenu:function(){var d=document.getElementById("sses2");if(!d){return}var j=d.getElementsByTagName("ul")[0];d.style.width=j.offsetWidth+1+"px";var f=d.getElementsByTagName("li");var e=d.getElementsByTagName("a");b=document.createElement("li");b.className="highlight";j.appendChild(b);var g=document.location.href.toLowerCase();a=-1;var k=-1;for(var h=0;h<e.length;h++){if(g.indexOf(e[h].href.toLowerCase())!=-1&&e[h].href.length>k){a=h;k=e[h].href.length}}if(a==-1&&/:\/\/(?:www\.)?[^.\/]+?\.[^.\/]+\/?$/.test){for(var h=0;h<e.length;h++){if(e[h].getAttribute("maptopuredomain")=="true"){a=h;break}}if(a==-1&&e[0].getAttribute("maptopuredomain")!="false"){a=0}}if(a>-1){b.style.width=f[a].offsetWidth+"px";sse2.move(f[a])}else{b.style.visibility="hidden"}for(var h=0;h<f.length-1;h++){f[h].onmouseover=function(){if(a==-1){b.style.visibility="visible"}if(this.offsetLeft!=b.offsetLeft){sse2.move(this)}}}d.onmouseover=function(){if(b.t2){b.t2=clearTimeout(b.t2)}};d.onmouseout=function(){if(a>-1&&f[a].offsetLeft!=b.offsetLeft){b.t2=setTimeout(function(){sse2.move(f[a])},50)}if(a==-1){b.t2=setTimeout(function(){b.style.visibility="hidden"},50)}}},move:function(e){clearInterval(b.timer);var d=(b.offsetLeft<e.offsetLeft)?1:-1;b.timer=setInterval(function(){sse2.mv(e,d)},15)},mv:function(e,d){if(d==1){if(b.offsetLeft-c<e.offsetLeft){this.changePosition(e,1)}else{clearInterval(b.timer);b.timer=setInterval(function(){sse2.recoil(e,1)},15)}}else{if(b.offsetLeft+c>e.offsetLeft){this.changePosition(e,-1)}else{clearInterval(b.timer);b.timer=setInterval(function(){sse2.recoil(e,-1)},15)}}this.changeWidth(e)},recoil:function(e,d){if(d==-1){if(b.offsetLeft>e.offsetLeft){b.style.left=e.offsetLeft+"px";clearInterval(b.timer)}else{b.style.left=b.offsetLeft+2+"px"}}else{if(b.offsetLeft<e.offsetLeft){b.style.left=e.offsetLeft+"px";clearInterval(b.timer)}else{b.style.left=b.offsetLeft-2+"px"}}},changePosition:function(e,d){if(d==1){b.style.left=b.offsetLeft+Math.ceil(Math.abs(e.offsetLeft-b.offsetLeft+c)/10)+1+"px"}else{b.style.left=b.offsetLeft-Math.ceil(Math.abs(b.offsetLeft-e.offsetLeft+c)/10)-1+"px"}},changeWidth:function(e){if(b.offsetWidth!=e.offsetWidth){var d=b.offsetWidth-e.offsetWidth;if(Math.abs(d)<4){b.style.width=e.offsetWidth+"px"}else{b.style.width=b.offsetWidth-Math.round(d/3)+"px"}}}}}();if(window.addEventListener){window.addEventListener("load",sse2.buildMenu,false)}else{if(window.attachEvent){window.attachEvent("onload",sse2.buildMenu)}else{window.onload=sse2.buildMenu}};
+
+
